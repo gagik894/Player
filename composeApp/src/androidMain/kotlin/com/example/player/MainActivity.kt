b9420b7997c33ee8af58b2592f8c8762944b9f48
gagik894 <gagiksargsyan894@gmail.com>
2025-10-02 23:03:52 +0200

    Request notification permission on Android Tiramisu and above
    - Add permission request for `POST_NOTIFICATIONS` on app startup.
    - Implement the logic in `MainActivity` to check for and request the permission on Android 13 (API 33) and higher.

--- composeApp/src/androidMain/kotlin/com/example/player/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/player/MainActivity.kt
@@ -1,19 +1,41 @@
 package com.example.player
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        askNotificationPermission()
 
         setContent {
             App()
+        }
+    }
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+        }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level 33 and above.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // Permission is already granted.
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
         }
     }
 }
